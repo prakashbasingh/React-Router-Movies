89fa655fd26e9d63fa60cb2ba93912eec2cfd06e
unknown <prakashbahadursingh2@gmail.com>
2020-04-21 02:47:37 -0700

    on SavedMovie Component, Link is replaced by NavLink

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Route, Switch, Link } from 'react-router-dom'
+import {NavLink, Route, Switch, Link } from 'react-router-dom'
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       // when movie card clicked it will display only clicked movie detail
-      <Link to = {`/movies/${movie.id}`}>
+      <NavLink to = {`/movies/${movie.id}`}>
         <span className="saved-movie">{movie.title}</span>
-      </Link>
+      </NavLink>
 
     ))}
     {/* no home button on home page, appears on movie detail page and when clicked it will bring home page back */}

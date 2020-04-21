01d65a7df8cef9133c68bef14ab0b4b5d94af9ec
unknown <prakashbahadursingh2@gmail.com>
2020-04-21 01:34:19 -0700

    link added to home butten. now, it does not display on home page, appear when movie card clicked to display movie detail and clicking on home button takes us to home page.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -5,12 +5,14 @@ const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
+      // when movie card clicked it will display only clicked movie detail
       <Link to = {`/movies/${movie.id}`}>
         <span className="saved-movie">{movie.title}</span>
       </Link>
 
     ))}
-    <Route path='/movies/:id'>
+    {/* no home button on home page, appears on movie detail page and when clicked it will bring home page back */}
+    <Route path='/movies/:id'> 
       <Link to='/'>
         <div className="home-button">Home</div>
       </Link>
